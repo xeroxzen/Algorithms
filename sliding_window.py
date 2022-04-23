d46a59d0a8690facacc56481fb99ac6a5c04a5e1
Google Jr <andilembele020@gmail.com>
2022-04-23 13:50:49 +0200

    Sliding window

--- sliding_window.py
+++ sliding_window.py
@@ -1,1 +1,19 @@
-def min_windo
+def contains_all(freq1,  freq2):
+    for char in freq2:
+        if freq1[char] < freq2[char]:
+            return False
+    return True
+
+def min_window(s, t):
+    n, m = len(s), let(t)
+    if m > n or m ==0:
+        return ""
+    freqt = Counter(t)
+    shortest = " " *(n+1)
+    for length in range(1, n+1):
+        for i in range(n-length+1):
+            sub = s[i:i+length]
+            freqs = Counter(sub)
+            if contains_all(freqs, freqt) and length < len(shortest):
+                shortest = sub
+    return shortest if len(shortest) <= n else ""
