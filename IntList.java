c4b61e799eafbf89fd232e1b28769aca02c9284e
Google Jr <andilembele020@gmail.com>
2022-07-29 09:03:58 +0200

    We did recursively, now we should do it iteratively

--- IntList.java
+++ IntList.java
@@ -7,9 +7,20 @@ public class IntList {
         rest = r;
     }
 
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        } else {
+            return 1 + this.rest.size();
+        }
+    }
+
     public static void main(String[] args) {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
+
+        System.out.println(L.size());
     }
 }
