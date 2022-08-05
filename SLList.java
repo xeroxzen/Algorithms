810f367fdafc39ce7e0e19529e8e231c2e82bbe1
Google Jr <andilembele020@gmail.com>
2022-08-05 13:03:49 +0200

    removed the secret language of the Gods to enable caching

--- SLList.java
+++ SLList.java
@@ -4,43 +4,73 @@
  */
 
 public class SLList {
-    // IntNode variable
-    // Note: Please do not mess with first
-    private IntNode first;
 
     // Nested classes
-    public class IntNode {
+    private static class IntNode {
         public int item;
         public IntNode next;
 
-        public IntNode(int f, IntNode r) {
-            item = f;
-            next = r;
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
         }
+    }
+
+    private IntNode first;
+    private int size;
+
+    // IntNode variable
+    private IntNode sentinel;
+
+    /** creates an empty SLList */
+    public SLList() {
+        sentinel = new IntNode(63, null);
+        size = 0;
     }
 
     // SLList constructor
     public SLList(int x) {
-
-        first = new IntNode(x, null);
+        sentinel = new IntNode(63, null);
+        sentinel.next = new IntNode(x, null);
+        size += 1;
     }
 
     // Method to add first element
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
+        size = size + 1;
     }
 
+    /** Returns the first item in the list. */
 
     public int getFirst() {
         return first.item;
     }
 
+    public void addLast(int x) {
+        size += 1;
+        IntNode p = first;
+
+        /** Move p until it reaches the end of the list. */
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+    }
+
+
+    public int size() {
+        return size;
+    }
+
     public static void main(String[] args) {
-        SLList L = new SLList(10);
+        SLList L = new SLList(15);
         L.addFirst(10);
         L.addFirst(5);
+        L.addLast(20);
 
-        int x = L.getFirst();
+        int x = L.size();
         System.out.println(x);
     }
 }
