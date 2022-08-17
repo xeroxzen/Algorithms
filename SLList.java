f51ffb385a54d10fbd7706dc69c43482170668ad
Google Jr <andilembele020@gmail.com>
2022-08-17 14:56:50 +0200

    Enter Generic Types to handle different data types

--- SLList.java
+++ SLList.java
@@ -1,46 +1,51 @@
 /**
  * An SLList is a list of integers, which hides the terrible truth
  * of the nakedness within
+ * What if we want to store different types of data in out our data structure,
+ * how do we go about changing... Enter Generics
  */
 
-public class SLList {
+public class SLList<Zen> {
 
     // Nested classes
-    private static class IntNode {
-        public int item;
-        public IntNode next;
+    private class StuffNode {
+        public Zen item;
+        public StuffNode next;
 
-        public IntNode(int i, IntNode n) {
-            item = i;
+        public StuffNode(Zen x, StuffNode n) {
+            item = x;
             next = n;
+        }
+
+        public StuffNode(int i, Object n) {
         }
     }
 
     /** The first item (if it exists) is at sentinel.next */
-    private IntNode sentinel;
+    private StuffNode sentinel;
     private int size;
 
     /** creates an empty SLList */
     public SLList() {
-        sentinel = new IntNode(63, null);
+        sentinel = new StuffNode(63, null);
         size = 0;
     }
 
     // SLList constructor
-    public SLList(int x) {
-        sentinel = new IntNode(63, null);
-        sentinel.next = new IntNode(x, null);
+    public SLList(Zen x) {
+        sentinel = new StuffNode(63, null);
+        sentinel.next = new StuffNode(x, null);
         size = 1;
     }
 
     // Method to add first element
-    public void addFirst(int x) {
-        sentinel.next = new IntNode(x, sentinel.next);
+    public void addFirst(Zen x) {
+        sentinel.next = new StuffNode(x, sentinel.next);
         size = size + 1;
     }
 
     /** Returns the first item in the list. */
-    public int getFirst() {
+    public Zen getFirst() {
         return sentinel.next.item;
     }
 
@@ -51,30 +56,23 @@ public class SLList {
         /**
          * This is a bit ugly of course.
          * if (sentinel == null) {
-         * sentinel = new IntNode(x, null);
+         * sentinel = new StuffNode(x, null);
          * return;
          * }
          */
 
-        IntNode p = sentinel;
+        StuffNode p = sentinel;
 
         /** Move p until it reaches the end of the list. */
         while (p.next != null) {
             p = p.next;
         }
 
-        p.next = new IntNode(x, null);
+        p.next = new StuffNode(x, null);
     }
 
     public int size() {
         return size;
     }
 
-    public static void main(String[] args) {
-        SLList L = new SLList();
-        L.addFirst(10);
-        L.addFirst(5);
-        L.addLast(20);
-        System.out.println(L.size());
-    }
 }
